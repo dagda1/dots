15ab6d87932d74350740e12bf46c5bc882e25868
Paul Cowan <paul.cowan@cutting.scot>
2023-09-16 08:47:19 +0100

    add iterm config

--- git-clean-up-repo.sh
+++ git-clean-up-repo.sh
@@ -4,22 +4,22 @@
 # Author: Rob Miller <rob@bigfish.co.uk>
 # Adapted from the original by Yorick Sijsling
 
-git checkout master &> /dev/null
+git checkout main &> /dev/null
 
-# Make sure we're working with the most up-to-date version of master.
+# Make sure we're working with the most up-to-date version of main.
 git fetch
 
 # Prune obsolete remote tracking branches. These are branches that we
 # once tracked, but have since been deleted on the remote.
 git remote prune origin
 
-# List all the branches that have been merged fully into master, and
-# then delete them. We use the remote master here, just in case our
-# local master is out of date.
-git branch --merged origin/master | grep -v 'master$' | xargs git branch -d
+# List all the branches that have been merged fully into main, and
+# then delete them. We use the remote main here, just in case our
+# local main is out of date.
+git branch --merged origin/main | grep -v 'main$' | xargs git branch -d
 
 # Now the same, but including remote branches.
-MERGED_ON_REMOTE=`git branch -r --merged origin/master | sed 's/ *origin\///' | grep -v 'master$'`
+MERGED_ON_REMOTE=`git branch -r --merged origin/main | sed 's/ *origin\///' | grep -v 'main$'`
 
 if [ "$MERGED_ON_REMOTE" ]; then
 	echo "The following remote branches are fully merged and will be removed:"
@@ -27,8 +27,8 @@ if [ "$MERGED_ON_REMOTE" ]; then
 
 	read -p "Continue (y/N)? "
 	if [ "$REPLY" == "y" ]; then
-		git branch -r --merged origin/master | sed 's/ *origin\///' \
-			| grep -v 'master$' | xargs -I% git push origin :% 2>&1 \
+		git branch -r --merged origin/main | sed 's/ *origin\///' \
+			| grep -v 'main$' | xargs -I% git push origin :% 2>&1 \
 			| grep --colour=never 'deleted'
 		echo "Done!"
 	fi
